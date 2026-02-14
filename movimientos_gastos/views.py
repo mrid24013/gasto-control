5c1d6381f908c228e3f311e80e491dce22070f37
Bryan Caballero <mrid24013@cesunbc.edu.mx>
2026-02-14 08:29:23 -0800

    changes

--- movimientos_gastos/views.py
+++ movimientos_gastos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import (
     CreateView
 )
-from .models import *
+from .models import Movimientos, Categorias
 from movimientos_gastos.formularios.forms import CategoriaForm, MovimientoForm
 from django.contrib.auth.mixins import LoginRequiredMixin #Para clases
 from django.contrib.auth.decorators import login_required #Para funciones
@@ -10,6 +10,13 @@ from django.contrib.auth.decorators import login_required #Para funciones
 @login_required(login_url='/')
 def home(request):
     return render(request, 'reportes.html')
+
+def index(request):
+    data = Movimientos.objects.all()
+    stu = {
+        "student_number": data
+    }
+    return render_to_response("login/profile.html", stu)
 
 class CreateViewCategoria(LoginRequiredMixin, CreateView):
     model = Categorias
